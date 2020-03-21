a1d6c9ab1f087808712877cbdd4af9cb591e1ae9
s00181793 <s00181793@mail.itsligo.ie>
2020-03-21 22:19:52 +0000

    Made a Movie class / controller

---
+++ Vidly/Vidly/Controllers/MoviesController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models; // using the movies from the model
+
+namespace Vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies/Random
+        //  what ever the methods name is
+        public ActionResult Random()    
+        {
+            //creating an instance of the model
+            var movie = new Movies()
+            {
+                Name="Shrek!"
+            };
+
+
+            return View(movie);  
+        }
+    }
+}
